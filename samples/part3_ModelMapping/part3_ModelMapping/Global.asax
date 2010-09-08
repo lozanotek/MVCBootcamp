51744692bcef501befa6f024ff40c64fca4e6a03
Javier Lozano <javier@lozanotek.com>
2010-09-08 02:46:38 -0500

    Added the Model Mapping sample.

---
+++ samples/part3_ModelMapping/part3_ModelMapping/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="part3_ModelMapping.MvcApplication" Language="C#" %>
---
+++ samples/part3_ModelMapping/part3_ModelMapping/Models/MappingExt.cs
@@ -0,0 +1,12 @@
+﻿namespace part3_ModelMapping.Models {
+    using AutoMapper;
+
+    public static class MappingExt {
+        static MappingExt() {
+            Mapper.CreateMap<Person, PersonViewModel>();
+        }
+        public static PersonViewModel ToViewModel(this Person person) {
+            return Mapper.Map<Person, PersonViewModel>(person);
+        }
+    }
+}
